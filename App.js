8bdec437f6c98aea371546906935ec212dc53ec8
lebkowskih@gmail.com <lebkowskih@gmail.com>
2023-02-26 15:12:16 +0100

    all countires rendered TODO: filter countires by name

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
 import Header from './components/Header';
 import CountryCard from './components/CountryCard';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+const [countries, setCountries] = useState([]);
+
+useEffect(() => {
+  fetch('https://restcountries.com/v3.1/all')
+    .then((response) => response.json())
+    .then((data) => setCountries(data))
+}, []);
+
   return (
-    <View>
+    <ScrollView>
       <Header></Header>
-      <CountryCard></CountryCard>
-    </View>
+      {countries.map((country, index) => (
+        <CountryCard
+          key={index}
+          name={country.name.common}
+          population={country.population}
+          region={country.region}
+          capital={country.capital}
+          flag={country.flags.png}
+        />
+      ))}
+    </ScrollView>
   );
 }
 
--- components/CountryCard.js
+++ components/CountryCard.js
@@ -1,18 +1,18 @@
 import { View, Text } from "react-native";
 import { Card } from "react-native-elements";
 
-const CountryCard = () => {
+const CountryCard = props => {
     return (
       <View>
         <Card>
-          <Card.Image source={{ uri: "https://flagcdn.com/w2560/de.png" }} />
-          <Card.Title>Niemcy</Card.Title>
-          <Text>Liczba mieszkańców: </Text>
-          <Text>Kontynent: </Text>
-          <Text>Stolica: </Text>
+          <Card.Image source={{ uri: props.flag }} />
+          <Card.Title>{props.name}</Card.Title>
+          <Text>Liczba mieszkańców: {props.population} </Text>
+          <Text>Kontynent: {props.region} </Text>
+          <Text>Stolica: {props.capital}</Text>
         </Card>
       </View>
     );
 }
 
-export default CountryCard
+export default CountryCard;
