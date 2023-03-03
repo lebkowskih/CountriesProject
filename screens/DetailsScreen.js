10603fd440b6cefb37d703943d05357329a9fbfd
lebkowskih <lebkowskih@gmail.com>
2023-03-03 18:05:21 +0100

    details page prototype; TODO: style it

--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -1,10 +1,30 @@
-import { ScrollView } from 'react-native';
-import { Text } from 'react-native-elements';
+import { ScrollView, StyleSheet } from 'react-native';
+import { Button, Image, Text } from 'react-native-elements';
 
-export default function DetailsScreen({navigation}) {
+export default function DetailsScreen({navigation, route}) {
     return (
         <ScrollView>
-            <Text>Hello!</Text>
+            <Text>{route.params.country.name.common}</Text>
+            <Text>{route.params.country.population}</Text>
+            <Text>{route.params.country.region}</Text>
+            <Text>{route.params.country.subregion}</Text>
+            <Text>{route.params.country.capital}</Text>
+            <Text>{route.params.country.topLevelDomain}</Text>
+            <Button 
+                text="<- Back"
+                onPress={() => navigation.navigate("Main")}
+            />
+            <Image 
+                style={styles.image}
+                source={{uri: route.params.country.flags.png}}
+            />
         </ScrollView>
     );
 }
+
+const styles = StyleSheet.create({
+    image: {
+        width:250,
+        height:250,
+    }
+})
--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -3,17 +3,11 @@ import CountryCard from '../components/CountryCard';
 import { useEffect, useState } from 'react';
 import FetchCountires from '../services/FetchCountries';
 import Header from '../components/Header'
-import DetailsScreen from './DetailsScreen';
-import { Button } from 'react-native-elements';
 
 export default function MainScreen ({navigation}) {
   const [countries, setCountries] = useState([]);
   const [inputValue, setInputValue] = useState('');
   const [region, setRegion] = useState('');
-
-  const goToDetails = () => {
-    navigation.navigate("Details");
-  }
 
   const handleInputChange = (value) => {
     setInputValue(value);
@@ -35,8 +29,7 @@ export default function MainScreen ({navigation}) {
     <ScrollView>
         <Header handleInputChange={handleInputChange} handleRegionSelect={handleRegionSelect}></Header>
         {countries.map((country, index) => (
-          <TouchableOpacity onPress={goToDetails} key={index}
-          >
+          <TouchableOpacity onPress={() => navigation.navigate("Details", {country: country})} key={index}>
             <CountryCard
               name={country.name.common}
               population={country.population}
